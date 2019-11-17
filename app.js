4df028604f30c8b22e90aeb0a009b3658d837e5e
Hamza <ham-zza4@live.co.uk>
2019-11-17 11:34:33 +0000

    new commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const request = require("request")
+const dotenv = require("dotenv").config()
+
+const address = process.argv[2]
+
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${process.env.API_KEY}`
+
+if (!address) {
+  return console.log("Please enter the name of the city")
+}
+
+request(url, (error, response, body) => {
+ const data = JSON.parse(body)
+ console.log(`It's currently ${data.main.temp} outside.`)
+})
